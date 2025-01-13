62ea5eb39eefadd24de92f985e8b13fccc68f018
vishmithSuranjaya <suranjaya0327@gmail.com>
2025-01-13 14:43:56 +0530

    ad posting backend completed-data saving to the db

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,8 +4,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home';
 import Admin from './Pages/Admin';
-import AdminRoutes from './Components/Admin Dashboard/AdminRoutes';
-import Properties from './Pages/Properties';
 import AdsSearchPage from './Pages/Properties';
 import AdPostingForm from './Components/AdPostingForm/AdPostingForm';
 
@@ -17,11 +15,11 @@ function App() {
      <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/login' element={<Login />} />
-        <Route path="/register" element={<Register />} />
+        {/* <Route path='/login' element={<Login />} /> */}
+        {/* <Route path="/register" element={<Register />} /> */}
         <Route path="/search_filter" element={<AdsSearchPage />} />
         <Route path='/admin' element={<Admin />} />
-        <Route path="/post" element={<AdPostingForm />} />
+        <Route path="/ad-posting-form" element={<AdPostingForm />} />
       </Routes>
      </BrowserRouter>
 
